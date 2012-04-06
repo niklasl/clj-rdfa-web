11b83e822504cb2244cc4106ee516c75c1ecb84a
Niklas Lindström <lindstream@gmail.com>
2012-04-06 15:02:48 +0200

    Use new rdfa.parser module

--- src/rdfa/web.clj
+++ src/rdfa/web.clj
@@ -6,21 +6,20 @@
             [ring.util.response :as resp]
             [compojure.route :as route]
             [compojure.handler :as handler])
-  (:require [rdfa.stddom :as impl]
-            [rdfa.repr :as repr]))
+  (:require (rdfa parser repr)))
 
 (defroutes main-routes
            (GET "/" []
                 (resp/resource-response "index.html" {:root "public"}))
            (GET "/extract.:ext" [ext url rdfagraph]
                 (let [{triples :triples
-                       proc-triples :proc-triples} (impl/get-rdfa url)
+                       proc-triples :proc-triples} (rdfa.parser/get-rdfa url)
                       result-triples (if (= rdfagraph "processor")
                                        proc-triples
                                        triples)
                       turtle-result (string/join
                                       "\n"
-                                      (map repr/repr-triple result-triples))
+                                      (map rdfa.repr/repr-triple result-triples))
                       mime-type (if (= ext "txt") "text/plain" "text/turtle")]
                   {:status 200
                    :headers {"Content-Type" (str mime-type "; charset=utf-8")}
